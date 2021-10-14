82b49fafc49aac49a62a300acba80ef12a4e9c02
Bill Mårtensson <bill@magictechnology.se>
2021-10-14 10:10:11 +0200

    piax

--- piaxv6tors/ViewController.swift
+++ piaxv6tors/ViewController.swift
@@ -9,11 +9,48 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var number1Textfield: UITextField!
+    
+    @IBOutlet weak var number2Textfield: UITextField!
+    
+    @IBOutlet weak var resultLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    
+    
+    @IBAction func doPlus(_ sender: Any) {
+        
+        // Gör om textfält till siffror
+        let siffra1 = Int(number1Textfield.text!)
+        let siffra2 = Int(number2Textfield.text!)
+        
+        
+        
+        var theresult = siffra1! + siffra2!
+        
+        resultLabel.text = String(theresult)
+    }
+    
+    
+    @IBAction func doMinus(_ sender: Any) {
+        
+        resultLabel.text = "hopp"
+    }
+    
+    
+    @IBAction func doMulti(_ sender: Any) {
+        
+    }
+    
+    @IBAction func doDiv(_ sender: Any) {
+        
+    }
+    
+    
 }
 
